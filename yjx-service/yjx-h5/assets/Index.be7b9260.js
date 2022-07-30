a1dac33c61407ee023b2dc95971209da0649ca5f
yangjunxiong <13667207700@163.com>
2022-07-30 15:25:34 +0800

    1

--- yjx-service/yjx-h5/assets/Index.be7b9260.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,x as r,b as n,e as t}from"./index.ea97895c.js";const s={name:"Index"};function a(c,_,p,d,f,i){const e=r("router-view");return n(),t(e)}var u=o(s,[["render",a]]);export{u as default};
