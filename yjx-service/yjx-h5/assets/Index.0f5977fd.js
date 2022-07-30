8697e02272b098e4ccd55897d66ef2ed5bfd22d1
yjx-show <39581450+yjx-show@users.noreply.github.com>
2022-07-30 14:57:36 +0800

    Add files via upload

---
+++ yjx-service/yjx-h5/assets/Index.0f5977fd.js
@@ -0,0 +1,1 @@
+import{_ as o,x as r,b as n,e as t}from"./index.2e944e3f.js";const s={name:"Index"};function a(c,_,p,d,f,i){const e=r("router-view");return n(),t(e)}var u=o(s,[["render",a]]);export{u as default};
